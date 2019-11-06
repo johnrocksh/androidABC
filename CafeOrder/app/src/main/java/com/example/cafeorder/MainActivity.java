022c9bb8eb4f3e0c32a730cfaadbfe1e6f64a15a
John Rock <ivanshevchenko@mail.ru>
2019-11-06 21:55:33 +0200

    working POST request

--- CafeOrder/app/src/main/java/com/example/cafeorder/MainActivity.java
+++ CafeOrder/app/src/main/java/com/example/cafeorder/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -27,13 +29,14 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-  private  String ServerUrl="http://start.webpower.cf/test/register/";
-    static  private HashMap<String,String> postDataParams;
+    private String registerUrl = "http://start.webpower.cf/test/register/";
+    private String authUrl=      "http://start.webpower.cf/test/auth/";
+
+    static private HashMap<String, String> postDataParams;
     TextView textViewName;
-    TextView textViewEmail;
     TextView textViewPassword;
 
-    LoginAndRegistration loginAndRegistration=null;
+    LoginAndRegistration loginAndRegistration = null;
 
 
     @Override
@@ -41,52 +44,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-           textViewName=findViewById(R.id.editTextName);
-           textViewEmail =findViewById(R.id.editTextEmail);
-           textViewPassword=findViewById(R.id.editTextPassword);
-     loginAndRegistration=new LoginAndRegistration();
 
+        textViewName = findViewById(R.id.editTextName);
+        textViewPassword = findViewById(R.id.editTextPassword);
+
+        loginAndRegistration = new LoginAndRegistration();
 
 
     }
 
- public void onClickRegistration(View view){
+    public void onClickRegistration(View view) {
 
-     postDataParams.put("name",textViewName.toString());
-     postDataParams.put("email",textViewEmail.toString());
-     postDataParams.put("password",textViewPassword.toString());
+        postDataParams=new HashMap<String,String>();
+        postDataParams.put("nickname", "john");
+        postDataParams.put("password", "salvation777");
+
+        loginAndRegistration.execute(authUrl);
 
 
-     loginAndRegistration.execute(ServerUrl);
+    }
 
-
- }
-
-    public static class  LoginAndRegistration extends AsyncTask<String,Void,String> {
-
+ static    public class LoginAndRegistration extends AsyncTask<String, Void, String> {
 
 
         @Override
         protected String doInBackground(String... strings) {
 
-           URL url=null;
-            HttpURLConnection urlConnection=null;
-            String response= "";
-
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            String response = "";
 
 
             try {
 
-                   url=new URL(strings[0]);
-                   HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                   conn.setReadTimeout(15000);
-                   conn.setConnectTimeout(15000);
-                   conn.setRequestMethod("POST");
-                   conn.setDoInput(true);
-                   conn.setDoOutput(true);
+                url = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
 
                 OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
                 writer.write(getPostDataString(postDataParams));
 
@@ -94,61 +94,58 @@ public class MainActivity extends AppCompatActivity {
                 writer.close();
                 os.close();
 
-                int responseCode=conn.getResponseCode();
+                int responseCode = conn.getResponseCode();
 
-                if (responseCode== HttpsURLConnection.HTTP_OK){
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
 
                     String line;
 
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-                    while((line=br.readLine())!=null){
+                    while ((line = br.readLine()) != null) {
 
 
-                        response+=line;
+                        response += line;
 
                     }
-                }
-                else{
+                } else {
 
-                    response="";
+                    response = "";
                 }
-
+                Log.i("response", response);
                 return response.toString();
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            finally{
+            } finally {
 
-            if(urlConnection!=null){
-
-                urlConnection.disconnect();
-            }
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
 
             }
 
 
             return null;
 
-       }
+        }
 
-        private String getPostDataString(HashMap<String,String>params) throws UnsupportedEncodingException {
+        private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
 
-            StringBuilder result=new StringBuilder();
-            boolean first= true;
-            for(Map.Entry<String,String> entry :params.entrySet()){
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+            for (Map.Entry<String, String> entry : params.entrySet()) {
 
                 if (first)
-                    first=false;
+                    first = false;
                 else
                     result.append("&");
 
-                result.append(URLEncoder.encode(entry.getKey(),"UTF-8"));
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
                 result.append("=");
-                result.append(URLEncoder.encode(entry.getValue(),"UTF-8"));
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
 
             }
             return result.toString();
